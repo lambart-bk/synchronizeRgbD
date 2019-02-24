d963e25a615e90afdb3b617a0d85bcf77b9b0315
lambart-bk <bk_074@foxmail.com>
2019-02-24 13:38:04 +0800

    fix execption block

--- src/synchronizeRgbD.cpp
+++ src/synchronizeRgbD.cpp
@@ -66,63 +66,64 @@ void callback_rgbd(   const sensor_msgs::ImageConstPtr &img_rgb ,
     //ROS_INFO("rgb stamp: %f", img_rgb->header.stamp.toSec());
     //ROS_INFO(" d  stamp: %f\n", img_dpt->header.stamp.toSec());	
     
+    cv_bridge::CvImagePtr cv_img_rgb;
+    cv_bridge::CvImagePtr cv_img_d;
     try
     {
       //std::cout<<"rgb encoding:\t"<<img_rgb->encoding<<std::endl;
       //std::cout<<"  d encoding:\t"<<img_dpt->encoding<<std::endl;
-      cv_bridge::CvImagePtr cv_img_rgb = cv_bridge::toCvCopy(img_rgb, sensor_msgs::image_encodings::BGR8);
-      cv_bridge::CvImagePtr cv_img_d   = cv_bridge::toCvCopy(img_dpt, sensor_msgs::image_encodings::TYPE_32FC1);
-      
-      cv::Mat mat_rgb   = cv_img_rgb->image.clone();
-      cv::Mat mat_d_raw =   cv_img_d->image.clone();
-      
-      cv::Mat mat_dpt(mat_d_raw.rows,mat_d_raw.cols,CV_16UC1);
-      double scale=1000.0;
-      for(int i=0;i<mat_d_raw.rows;i++)
-	for(int j=0;j<mat_d_raw.cols;j++)
-	{
-	  float ov=mat_d_raw.at<float>(i,j);
-	  unsigned short nv=ov*scale;
-  	  //std::cout<<"\t"<<nv<<" ";//std::endl;
-	  mat_dpt.at<unsigned short>(i,j)=nv;
-	} 
-	
-	
-      int64_t timestamp=img_rgb->header.stamp.toNSec();  
-//       std::cout<<"timestamp:\t"<<timestamp<<std::endl;
-      unsigned char * rgbData = 0;
-      unsigned char * depthData = 0;
-      unsigned long depthSize = depth_compress_buf_size;
-      int32_t rgbSize = 0;
-	
-      boost::thread_group threads;
-      threads.add_thread(new boost::thread(compress2,
-					    depth_compress_buf,
-					    &depthSize,
-					    (const Bytef*)mat_dpt.data,
-					    width * height * sizeof(short),
-					    Z_BEST_SPEED));
-      threads.add_thread(new boost::thread(boost::bind(&encodeJpeg,
-							(cv::Vec<unsigned char, 3> *)mat_rgb.data)));
-      threads.join_all();
-
-      rgbSize = encodedImage->width;
-      depthData = (unsigned char *)depth_compress_buf;
-      rgbData = (unsigned char *)encodedImage->data.ptr;
-      
-      fwrite(&timestamp,sizeof(int64_t),1,logfile);
-      fwrite((int32_t *)&depthSize,sizeof(int32_t),1,logfile);
-      fwrite(&rgbSize,sizeof(int32_t),1,logfile);
-      fwrite(depthData,depthSize,1,logfile);
-      fwrite(rgbData,rgbSize,1,logfile);
-      numFrames++;
-         
+      cv_img_rgb = cv_bridge::toCvCopy(img_rgb, sensor_msgs::image_encodings::BGR8);
+      cv_img_d   = cv_bridge::toCvCopy(img_dpt, sensor_msgs::image_encodings::TYPE_32FC1);
     }
     catch(cv_bridge::Exception &e)
     {
       ROS_ERROR("cv_bridge  Exception %s",e.what());
     }
+      
+    cv::Mat mat_rgb   = cv_img_rgb->image.clone();
+    cv::Mat mat_d_raw =   cv_img_d->image.clone();
     
+    cv::Mat mat_dpt(mat_d_raw.rows,mat_d_raw.cols,CV_16UC1);
+    double scale=1000.0;
+    for(int i=0;i<mat_d_raw.rows;i++)
+      for(int j=0;j<mat_d_raw.cols;j++)
+      {
+	float ov=mat_d_raw.at<float>(i,j);
+	unsigned short nv=ov*scale;
+	//std::cout<<"\t"<<nv<<" ";//std::endl;
+	mat_dpt.at<unsigned short>(i,j)=nv;
+      } 
+      
+      
+    int64_t timestamp=img_rgb->header.stamp.toNSec();  
+//       std::cout<<"timestamp:\t"<<timestamp<<std::endl;
+    unsigned char * rgbData = 0;
+    unsigned char * depthData = 0;
+    unsigned long depthSize = depth_compress_buf_size;
+    int32_t rgbSize = 0;
+      
+    boost::thread_group threads;
+    threads.add_thread(new boost::thread(compress2,
+					  depth_compress_buf,
+					  &depthSize,
+					  (const Bytef*)mat_dpt.data,
+					  width * height * sizeof(short),
+					  Z_BEST_SPEED));
+    threads.add_thread(new boost::thread(boost::bind(&encodeJpeg,
+						      (cv::Vec<unsigned char, 3> *)mat_rgb.data)));
+    threads.join_all();
+
+    rgbSize = encodedImage->width;
+    depthData = (unsigned char *)depth_compress_buf;
+    rgbData = (unsigned char *)encodedImage->data.ptr;
+    
+    fwrite(&timestamp,sizeof(int64_t),1,logfile);
+    fwrite((int32_t *)&depthSize,sizeof(int32_t),1,logfile);
+    fwrite(&rgbSize,sizeof(int32_t),1,logfile);
+    fwrite(depthData,depthSize,1,logfile);
+    fwrite(rgbData,rgbSize,1,logfile);
+    numFrames++;
+           
 }
 
 
